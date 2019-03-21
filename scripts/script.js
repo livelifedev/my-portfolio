01d5148ccc022de3b72162a6c4feff5b0643c4b9
johnr <johnrubio93@gmail.com>
2019-03-21 11:06:32 +1100

    added css and js for chart animation, added links and icons for contact section

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,6 @@ const body = document.querySelector("body");
 var navHeight = 85;
 var navTop = window.innerHeight;
 
-window.addEventListener("resize", () => { 
-  navTop = window.innerHeight;
-});
-
-window.addEventListener("scroll", () => {
-  stickyNavigation();
-});
-
 function stickyNavigation() {
   if(window.pageYOffset >= navTop - navHeight) {
     body.classList.add("sticky");
@@ -17,3 +9,21 @@ function stickyNavigation() {
     body.classList.remove("sticky");
   }
 }
+
+function chartAnim() {
+  const skillsTop = document.getElementById("skills").offsetTop - 300;
+  if(window.pageYOffset >= skillsTop) {
+    body.classList.add("loaded");
+  } else {
+    body.classList.remove("loaded");
+  }
+}
+
+window.addEventListener("resize", () => { 
+  navTop = window.innerHeight;
+});
+
+window.addEventListener("scroll", () => {
+  stickyNavigation();
+  chartAnim();
+});
